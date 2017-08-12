c930379bb065cc60ff8472a5e6e54be7a8e9588d
Reginaldo Morikawa <reginaldo@wolfandpartners.com.br>
2017-08-12 03:29:56 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exemplo1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Olá, este é meu primeiro programa");
+        }
+    }
+}
